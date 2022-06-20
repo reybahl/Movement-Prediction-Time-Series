46c630697d70bf50c1d91eaa2738fd3103a4e53d
ReyBahl <bahl.reyansh@gmail.com>
2022-06-20 09:46:34 -0400

    split into train and test before feature extraction

--- combine_dfs.py
+++ combine_dfs.py
@@ -4,21 +4,36 @@ import sys
 import numpy as np
 import pandas as pd
 
-data_path = ' '.join(sys.argv[1:]) # enter dataset path from command line
+from sklearn.model_selection import train_test_split
+
+data_path = 'Dataset' # enter dataset path from command line
 
 target_df = pd.read_csv(os.path.join(data_path, 'dataset', 'MovementAAL_target.csv'))
 
 labels = target_df[' class_label']
 labels.rename('class_label', inplace=True) # remove the leading whitespace
 
-main_df = pd.DataFrame()
+X_train = pd.DataFrame()
+X_test = pd.DataFrame()
+
 sequence_ids = target_df['#sequence_ID']
 
-for sequence in sequence_ids:
+train_ids, test_ids, train_labels, test_labels = train_test_split(sequence_ids, labels, test_size=0.2)
+
+for sequence in train_ids:
     df = pd.read_csv(os.path.join(data_path, 'dataset', f'MovementAAL_RSS_{sequence}.csv'))
     df.insert(0, 'sequence', sequence-1)
     df['step'] = np.arange(df.shape[0])
-    main_df = pd.concat([main_df, df])
+    X_train = pd.concat([X_train, df])
 
-main_df.to_csv('data.csv', index=False)
-labels.to_csv('labels.csv', index=False)
+for sequence in test_ids:
+    df = pd.read_csv(os.path.join(data_path, 'dataset', f'MovementAAL_RSS_{sequence}.csv'))
+    df.insert(0, 'sequence', sequence-1)
+    df['step'] = np.arange(df.shape[0])
+    X_test = pd.concat([X_test, df])
+
+X_train.to_csv('train.csv', index=False)
+X_test.to_csv('test.csv', index=False)
+
+train_labels.to_csv('train_labels.csv', index=False)
+test_labels.to_csv('test_labels.csv', index=False)
