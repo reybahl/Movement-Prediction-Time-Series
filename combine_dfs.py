dc17ea3b7dca6d0956f11c321c57cd7b19aba9ad
ReyBahl <bahl.reyansh@gmail.com>
2022-06-19 22:50:16 -0400

    update data generation code, add requirements.txt, begin method 1

--- combine_dfs.py
+++ combine_dfs.py
@@ -7,15 +7,18 @@ import pandas as pd
 data_path = ' '.join(sys.argv[1:]) # enter dataset path from command line
 
 target_df = pd.read_csv(os.path.join(data_path, 'dataset', 'MovementAAL_target.csv'))
+
 labels = target_df[' class_label']
+labels.rename('class_label', inplace=True) # remove the leading whitespace
 
 main_df = pd.DataFrame()
-sequence_ids = list(target_df['#sequence_ID'])
+sequence_ids = target_df['#sequence_ID']
 
 for sequence in sequence_ids:
     df = pd.read_csv(os.path.join(data_path, 'dataset', f'MovementAAL_RSS_{sequence}.csv'))
-    df.insert(0, 'sequence', sequence)
+    df.insert(0, 'sequence', sequence-1)
     df['step'] = np.arange(df.shape[0])
     main_df = pd.concat([main_df, df])
 
 main_df.to_csv('data.csv', index=False)
+labels.to_csv('labels.csv', index=False)
